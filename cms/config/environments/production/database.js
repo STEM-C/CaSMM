4ea89b96baec1c83f29936df7013809d7c19de1f
nionata <n.ionata129@gmail.com>
2020-05-13 07:13:28 -0400

    database connection settings

--- cms/config/environments/production/database.js
+++ cms/config/environments/production/database.js
@@ -13,11 +13,14 @@ if (process.env.DATABASE_URL) {
   const parsed = url.parse(process.env.DATABASE_URL, true);
   const [username, password] = parsed.auth.split(':');
 
-  settings.host     = parsed.hostname;
-  settings.port     = Number(parsed.port);
+  settings.host = parsed.hostname;
+  settings.port = Number(parsed.port);
   settings.database = parsed.pathname.substr(1);
   settings.username = username;
   settings.password = password;
+  settings.ssl = {
+    "rejectUnauthorized": false
+  };
 }
 
 module.exports = {
@@ -26,7 +29,15 @@ module.exports = {
     default: {
       connector: 'bookshelf',
       settings,
-      options: {}
+      options: {
+        "pool": {
+          "min": 0,
+          "max": 15,
+          "idleTimeoutMillis": 30000,
+          "createTimeoutMillis": 30000,
+          "acquireTimeoutMillis": 30000
+        }
+      }
     }
   }
 };
