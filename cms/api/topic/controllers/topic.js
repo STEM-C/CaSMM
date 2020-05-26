0a27305ccfcf549888190f900c8d75d63053f580
nionata <n.ionata129@gmail.com>
2020-05-26 11:00:17 -0400

    topics now has activities

--- cms/api/topic/controllers/topic.js
+++ cms/api/topic/controllers/topic.js
@@ -1,8 +1,21 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils')
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+
+        let entities;
+
+        if (ctx.query._q) {
+            entities = await strapi.services.topic.search(ctx.query)
+        } else {
+            entities = await strapi.services.topic.find(ctx.query, [
+                "activities.difficulty",
+                "activities.learning_category"
+            ])
+        }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.topic }))
+    },
+}
