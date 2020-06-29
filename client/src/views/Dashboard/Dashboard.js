9d08209573502b4c19a99fd25a0a40bf9ff54879
Adam Tamargo <tamargoadam@gmail.com>
2020-06-29 13:16:55 -0400

    align table items

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,49 +1,54 @@
 import React, { useEffect, useState } from "react"
-import { getTopics, getSchools, getClassrooms } from "../../Utils/requests"
+import { getSchools, getClassrooms } from "../../Utils/requests"
 import { removeUserSession, getUser } from "../../Utils/AuthRequests";
-import { Table } from 'antd';
+import { Table, Switch } from 'antd';
 import './Dashboard.less'
 
 import Header from '../../components/Header.js'
 
 export default function Dashboard(props) {
-    const [activities, setActivities] = useState([])
     const [tableData, setTableData] = useState([])
-    const [selectedRowKeys, setSelectedRowKeys] = useState([])
     const user = getUser();
+
+    function onChange(checked) {
+        console.log(`switch to ${checked}`);
+    }
 
     const columns = [
         {
             title: 'Classroom',
             dataIndex: 'classroom',
-            key: 'classroom'
+            key: 'classroom',
+            align: 'left'
         },
         {
             title: 'Session Name',
             dataIndex: 'session',
-            key: 'session'
+            key: 'session',
+            align: 'right'
         },
         {
             title: 'Description',
             dataIndex: 'description',
-            key: 'description'
+            key: 'description',
+            align: 'right'
         },
         {
             title: 'Join Code',
             dataIndex: 'code',
-            key: 'code'
+            key: 'code',
+            align: 'right'
+        },
+        {
+            title: 'Enable/Disable Session',
+            dataIndex: 'active',
+            key: 'active',
+            align: 'right',
+            render: (e) => (< Switch  onChange={onChange} defaultChecked={e} />)
         }
     ];
 
     useEffect(() => {
-
-        getTopics(sessionStorage.getItem('token')).then(topics => {
-            // temporary - put all the activities into one array for current rendering
-            let activities = []
-            topics.forEach(topic => activities = activities.concat(topic.activities))
-
-            setActivities(activities)
-        })
         getSchools(sessionStorage.getItem('token')).then(schoolArray => {
             let data = [];
             schoolArray.map( school => {
@@ -55,7 +60,8 @@ export default function Dashboard(props) {
                                 classroom: classroom.name,
                                 session: session.name,
                                 description: session.description,
-                                code: session.code
+                                code: session.code,
+                                active: session.active
                             })
                         })
                     });
@@ -74,7 +80,7 @@ export default function Dashboard(props) {
         <div id={"container"}>
             <Header user={user.username} handleLogout={handleLogout}/>
             <div id='table-container'>
-                <Table rowSelection={selectedRowKeys} columns={columns} dataSource={tableData}/>
+                <Table columns={columns} dataSource={tableData}/>
             </div>
         </div>
     )
