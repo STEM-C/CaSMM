a04104cc1295cdc5478145cf45561650729720d2
Nick <n.ionata129@gmail.com>
2020-06-29 08:18:00 -0400

    moved frontend middleware settings to middleware config

--- cms/config/middleware.js
+++ cms/config/middleware.js
@@ -1,16 +1,20 @@
 module.exports = {
     timeout: 100,
-    load: {
-        before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
-        order: [
-            'Define the middlewares\' load order by putting their name in this array is the right order',
-        ],
-        after: ['parser', 'router'],
-    },
     settings: {
         public: {
             path: './public',
             maxAge: 60000,
+        },
+        frontend: {
+            enabled: true,
+            path: './public/frontend',
+            routes: [
+                '/',
+                '/login',
+                '/register',
+                '/teacher*',
+                '/workspace*'
+            ]
         },
         // logger: {
         //     // dev + prod
