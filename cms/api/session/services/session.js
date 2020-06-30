85d83f7aed6427daac0c589709ce9ad6745e0fd6
Nick <n.ionata129@gmail.com>
2020-06-30 09:51:28 -0400

    added custom update logic and moved the util functions into session service

--- cms/api/session/services/session.js
+++ cms/api/session/services/session.js
@@ -1,8 +1,30 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
+// find a session by code
+module.exports.findByCode = (code) => strapi.query('session').findOne({ code })
 
-module.exports = {};
+// TODO: add a maximum number of tries
+// generate a code that is unique amongst active sessions
+module.exports.getUniqueCode = async (currentCode = undefined) => {
+
+    let code = currentCode
+    let codeExists
+    do {
+        // allow an existing code to be passed,
+        // checking if it active and returning
+        // a new code if it is being used
+        if (!code) {
+            // get a four digit code
+            const now = Date.now().toString()
+            const len = now.length
+            code = now.substr(len - 4, len)
+        }
+
+        // check if an active session is using the code
+        const sessions = await strapi.query('session').findOne({ code, active: true })
+        codeExists = sessions !== null
+    }
+    while (codeExists)
+
+    return code
+}
