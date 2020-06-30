d174023d784655957e6dbd2f9ae77dfa8106ace8
Nick <n.ionata129@gmail.com>
2020-06-30 14:10:08 -0400

    added policy to create mentor and added custom logic

--- cms/api/mentor/controllers/mentor.js
+++ cms/api/mentor/controllers/mentor.js
@@ -12,5 +12,31 @@ module.exports = {
 
         // remove private fields and return the mentor
         return sanitizeEntity(mentor, { model: strapi.models.mentor })
+    },
+
+    /**
+     * Create a new mentor
+     * 
+     * @param {String} first_name
+     * @param {String} last_name
+     *
+     * @return {Mentor} 
+     */
+    async create(ctx) {
+
+        // ensure the request is in the right format
+        if (ctx.is('multipart')) return ctx.badRequest(
+            'Multipart requests are not accepted!',
+            { id: 'Mentor.create.format.invalid', error: 'ValidationError' }
+        )
+
+        // get the user that is currently logged in
+        // to set as the new mentor's user
+        const { user } = ctx.state
+        ctx.request.body.user = user.id
+        
+        // remove private fields and return the new mentor
+        const mentor = await strapi.services.mentor.create(ctx.request.body)
+        return sanitizeEntity(mentor, { model: strapi.models.mentor })
     }
 }
