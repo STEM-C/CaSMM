90d65d1084a1ccdebd14380e04e398cccd8a2757
Adam Tamargo <tamargoadam@gmail.com>
2020-07-07 13:16:06 -0400

    remove useCallback

--- client/src/views/StudentLogin/StudentLogin.js
+++ client/src/views/StudentLogin/StudentLogin.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import './StudentLogin.less'
 import Logo from "../../assets/casmm_logo.png"
 import {getStudents, postJoin} from "../../Utils/requests";
