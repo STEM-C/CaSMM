8974700740ecb2257abc0f660984fe6866a2f469
Nick <n.ionata129@gmail.com>
2020-07-07 12:32:02 -0400

    added sandbox toolbox endpoint

--- cms/api/activity/controllers/activity.js
+++ cms/api/activity/controllers/activity.js
@@ -1,48 +1,24 @@
 'use strict'
 
-const blocksToToolbox = (blocks) => {
-
-    let toolbox = {}
-    blocks.forEach(block => {
-
-        const { blocks_category, name, description } = block
-        if (!blocks_category) return 
-
-        let sanitizedBlock = { name, description }
-
-        // append the block to an existing category
-        // else create a new category
-        if (toolbox[blocks_category.name]) {
-            toolbox[blocks_category.name].push(sanitizedBlock)
-        } else {
-            toolbox[blocks_category.name] = [sanitizedBlock]
-        }
-    })
-
-    return Object.entries(toolbox)
-}
-
 module.exports = {
 
     async toolbox(ctx) {
 
         const { id } = ctx.params
 
-        let result = await strapi
-            .query('block').model // gets the underlying bookshelf model
-            .query(qb => { // get only the blocks belonging to the activity
-                qb.join('activities__blocks', 'blocks.id', 'activities__blocks.block_id')
-                  .where('activities__blocks.activity_id', id)
-            })
-            .fetchAll({ // get the related blocks_category data
-                withRelated: ['blocks_category']
-            })
+        // get the blocks 
+        const blocks = id ? 
+            await strapi.services.block.findByActivity(id) : 
+            await strapi.services.block.find()
 
-        let response = {
+        // return 404 if blocks is undefined
+        // (only the case of an activity not existing)
+        if (!blocks) return undefined
+
+        // return the activity id and the toolbox
+        return {
             id,
-            toolbox: blocksToToolbox(result.toJSON())
+            toolbox: strapi.services.block.blocksToToolbox(blocks)
         }
-
-        return response
     },
 }
