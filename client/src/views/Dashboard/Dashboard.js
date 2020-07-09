b5ca76d79eee60d59de1bc2650102356679d0036
Adam Tamargo <tamargoadam@gmail.com>
2020-07-09 12:45:00 -0400

    begin redoing mentor view with class cards

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,12 +1,55 @@
 import React, {useEffect, useState} from "react"
 import {getMentor, getClassrooms, updateSession} from "../../Utils/requests"
 import {removeUserSession, getUser, getToken} from "../../Utils/AuthRequests";
-import {Table, Switch} from 'antd';
+import {Table, Switch, Card} from 'antd';
 import './Dashboard.less'
 
 import Header from '../../components/Header.js'
 
 export default function Dashboard(props) {
+    const [classrooms, setClassrooms] = useState([])
+    const user = getUser();
+
+    useEffect( () => {
+        let classroomIds = []
+        getMentor(getToken()).then(mentor => {
+            mentor.classrooms.forEach(classroom => {
+                classroomIds.push(classroom.id)
+            });
+            getClassrooms(classroomIds, getToken()).then(classrooms => {
+                setClassrooms(classrooms)
+            });
+        })
+    }, []);
+
+    const handleLogout = () => {
+        removeUserSession();
+        props.history.push('/');
+    };
+
+    return (
+        <div className="container">
+            <Header user={user.username} handleLogout={handleLogout}/>
+            <h1 id='page-header'>Your Classrooms:</h1>
+            <div id='card-container' className='flex flex-row'>
+                {console.log(classrooms)}
+                {classrooms.map(classroom =>
+                    <Card id='card' title={classroom.name}>
+                        <div id='card-content-container'>
+                            <p>Number of students: {classroom.students.length}</p>
+                        </div>
+                        <div id='card-button-container' className='flex flex-row'>
+                            <button>View Roster</button>
+                            <button></button>
+                        </div>
+                    </Card>
+                )}
+            </div>
+        </div>
+    )
+}
+
+/*export default function Dashboard(props) {
     const [tableData, setTableData] = useState([]);
     const [classFilters, setClassFilters] = useState([]);
     const [filteredInfo, setFilteredInfo] = useState([]);
@@ -118,4 +161,4 @@ export default function Dashboard(props) {
             </div>
         </div>
     )
-}
+}*/
