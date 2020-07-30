38ec4452205bfd95a3f2b19ecb3e1c6c5a7c0b14
Nick <n.ionata129@gmail.com>
2020-07-30 17:55:24 -0400

    create findByDay and init create save

--- server/api/save/controllers/save.js
+++ server/api/save/controllers/save.js
@@ -1,8 +1,49 @@
-'use strict';
+'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+    /**
+     * Get the current student(s) saves for a day
+     * 
+     * @param {*} ctx 
+     */
+    async findByDay(ctx) {
+
+        const { ids } = ctx.state.user
+        const { day } = ctx.params
+        console.log(ids, day)
+
+        const saves = await strapi.services.save.find({ student: ids, day })
+        return saves
+    },
+
+    async create(ctx) {
+
+        // ensure request was not sent as formdata
+        if (ctx.is('multipart')) return ctx.badRequest(
+            'Multipart requests are not accepted!',
+            { id: 'Save.create.format.invalid', error: 'ValidationError' }
+        )
+
+        // ensure the request has the right number of params
+        const params = Object.keys(ctx.request.body).length
+        if (params !== 2) return ctx.badRequest(
+            'Invalid number of params!',
+            { id: 'Save.create.body.invalid', error: 'ValidationError' }
+        )
+
+        // validate the request
+        const { day, workspace } = ctx.request.body
+        if (!strapi.services.validator.isInt(day) || !workspace) return ctx.badRequest(
+            'A day and workspace must be provided!',
+            { id: 'Save.create.body.invalid', error: 'ValidationError' }
+        )
+
+        // ensure the day is valid
+        const validDay = await strapi.services.day.findOne({ id: day })
+        if (validDay === null) return ctx.notFound(
+            'The day provided is invalid!',
+            { id: 'Save.create.day.invalid', error: 'ValidationError' }
+        )
+    }
+}
