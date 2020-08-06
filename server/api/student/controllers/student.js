6f68edc1a0f07d75cb1ed1fd033fee80ca6379d5
Adam Tamargo <tamargoadam@gmail.com>
2020-08-06 16:08:53 -0400

    allow POST /students to take array of students or single student

--- server/api/student/controllers/student.js
+++ server/api/student/controllers/student.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const { sanitizeEntity } = require("strapi-utils/lib")
+const {sanitizeEntity} = require("strapi-utils/lib")
 
 module.exports = {
 
     async me(ctx) {
 
         // get the student that is currently logged in
-        const { ids, session } = ctx.state.user
-        const students = await strapi.services.student.find({ id: ids }, [])
+        const {ids, session} = ctx.state.user
+        const students = await strapi.services.student.find({id: ids}, [])
 
         // return the students and the current session
         return {
             session,
-            students: students.map(student => sanitizeEntity(student, { model: strapi.models.student }))
+            students: students.map(student => sanitizeEntity(student, {model: strapi.models.student}))
         }
     },
     /**
@@ -28,35 +28,51 @@ module.exports = {
         // ensure request was not sent as formdata
         if (ctx.is('multipart')) return ctx.badRequest(
             'Multipart requests are not accepted!',
-            { id: 'Student.enrolled.format.invalid', error: 'ValidationError' }
+            {id: 'Student.enrolled.format.invalid', error: 'ValidationError'}
         )
 
         // ensure the request has the right number of params
         const params = Object.keys(ctx.request.body).length
         if (params !== 1) return ctx.badRequest(
             'Invalid number of params!',
-            { id: 'Student.enrolled.body.invalid', error: 'ValidationError' }
+            {id: 'Student.enrolled.body.invalid', error: 'ValidationError'}
         )
 
         // validate the request
-        const { enrolled } = ctx.request.body
+        const {enrolled} = ctx.request.body
         if (typeof enrolled !== "boolean") return ctx.badRequest(
             'An enrollment status must be provided!',
-            { id: 'Student.enrolled.body.invalid', error: 'ValidationError' }
+            {id: 'Student.enrolled.body.invalid', error: 'ValidationError'}
         )
 
         // find student
-        const { id } = ctx.params
-        let student = await strapi.services.student.findOne({ id: id })
+        const {id} = ctx.params
+        let student = await strapi.services.student.findOne({id: id})
         if (!student) return ctx.notFound(
             'The student id provided does not correspond to a valid student!',
-            { id: 'Student.enrolled.id.invalid', error: 'ValidationError' }
+            {id: 'Student.enrolled.id.invalid', error: 'ValidationError'}
         )
 
 
         // remove private fields and return the new student
-        student.enrolled =  ctx.request.body.enrolled
+        student.enrolled = ctx.request.body.enrolled
         const updatedStudent = await strapi.services.student.update({id: id}, student)
-        return sanitizeEntity(updatedStudent, { model: strapi.models.student })
+        return sanitizeEntity(updatedStudent, {model: strapi.models.student})
     },
+
+    async create(ctx) {
+        const {students, classroom} = ctx.request.body
+
+        if (students) {
+            return await Promise.all(students.map(student => {
+                return strapi.services.student.create({
+                    name: student.name,
+                    character: student.character,
+                    classroom: classroom
+                })
+            }))
+        }
+
+        return strapi.services.student.create(ctx.request.body)
+    }
 }
