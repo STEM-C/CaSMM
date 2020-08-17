2651ab67b1b2ef4b8e2e98e88c2a42e874338743
Nick <n.ionata129@gmail.com>
2020-08-17 15:57:42 -0400

    init get submission and changed create submission logic

--- server/api/submission/services/submission.js
+++ server/api/submission/services/submission.js
@@ -1,8 +1,26 @@
-'use strict';
+'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
+const progressStatusMap = {
+    0: 'WAITING',
+    50: 'COMPILING',
+    100: 'COMPLETED'
+}
 
-module.exports = {};
+module.exports.updateProgress = async (jobId, progress) => {
+
+    // get the submission_id from the job
+    const job = await strapi.connections.compile_queue.getJob(jobId)
+
+    // construct the update object
+    let updates = { status: progressStatusMap[progress] }
+    if (progress == 0) updates.job_id = jobId
+
+    // update the submission
+    await strapi.services.submission.update({ id: job.data.submission_id }, updates)
+
+    console.log(`Compile job ${jobId} is ${updates.status}`)
+}
+
+module.exports.completeJob = (jobId, result) => {
+    console.log(`${jobId}, ${result}`)
+}
