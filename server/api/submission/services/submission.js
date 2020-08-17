0679ea1e7bd2168dd74e06eda306f5421901a109
Nick <n.ionata129@gmail.com>
2020-08-17 16:03:25 -0400

    move init queue to submission service

--- server/api/submission/services/submission.js
+++ server/api/submission/services/submission.js
@@ -1,11 +1,33 @@
 'use strict'
 
+const Queue = require('bull')
+
 const progressStatusMap = {
     0: 'WAITING',
     50: 'COMPILING',
     100: 'COMPLETED'
 }
 
+// connect/create the submissions queue, attach listeners, and set global listeners
+module.exports.initCompileQueue = () => {
+
+    // get queue url
+    const url = strapi.config.get('compile_queue.url')
+
+    // connect to queue
+    const compile_queue = new Queue('submissions', url);
+
+    // add the submission progress listener
+    compile_queue.on('global:progress', strapi.services.submission.updateProgress)
+      
+    // add the submission complete listener 
+    compile_queue.on('global:complete', strapi.services.submission.completeJob)
+
+    // add queue globally
+    strapi.connections.compile_queue = compile_queue
+} 
+
+// listener function for queue progress updates
 module.exports.updateProgress = async (jobId, progress) => {
 
     // get the submission_id from the job
@@ -21,6 +43,7 @@ module.exports.updateProgress = async (jobId, progress) => {
     console.log(`Compile job ${jobId} is ${updates.status}`)
 }
 
+// listener function for queue completed jobs
 module.exports.completeJob = (jobId, result) => {
     console.log(`${jobId}, ${result}`)
 }
