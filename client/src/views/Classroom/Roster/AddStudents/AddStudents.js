2120a37946019266c06af84421c3c9cb64be91a0
Adam Tamargo <tamargoadam@gmail.com>
2020-08-18 17:00:43 -0400

    require formatted name entry; parse names to 'First L.' format

--- client/src/views/Classroom/Roster/AddStudents/AddStudents.js
+++ client/src/views/Classroom/Roster/AddStudents/AddStudents.js
@@ -69,16 +69,25 @@ export default function AddStudents(props) {
 
     const handleOnDrop = async (roster) => {
         // on file select, filter out bad data and set uploadedRoster and tableData
+        let badInput = false;
         const filteredRoster = roster.filter(student => {
-            if (student.data.name) return true;
+            // verify name is in format "Last, First Middle"
+            if (student.data.name) {
+                if (student.data.name.search('([A-Za-z]+),\\s*([A-Za-z]+)\\s*([A-Za-z]+)') > -1) return true;
+                badInput = true
+            }
             return false
         });
 
-        const students = await filteredRoster.map(student => student.data);
+        const students = await filteredRoster.map(student => {
+            let names = student.data.name.split(" ");
+            let name = `${names[1]} ${names[0].substring(0, 1)}.`;
+            return {name: name, animal: student.data.animal}
+        });
         setUploadedRoster(students);
         const data = await getTableData(students);
         setTableData(data);
-        if (students.length === 0) message.warning(
+        if (badInput || students.length === 0) message.warning(
             "There may have been an issue parsing one or more data entries in the uploaded CSV. " +
             " Please verify that your data is in the specified format.", 8)
     };
@@ -110,16 +119,17 @@ export default function AddStudents(props) {
             <div id='manual-input'>
                 <h3>Manual Input:</h3>
                 <form>
-                    <input type="text" value={name} onChange={e => {setName(e.target.value)}}
+                    <input type="text" value={name} onChange={e => {
+                        setName(e.target.value)
+                    }}
                            id="name" name="name" placeholder='Student Name'/>
-                    {/*<input type="text" {...animal} id="animal" name="animal" placeholder='Student Animal'/>*/}
                     <div id='emoji-picker'>
                         {chosenCharacter ? (
                             <span>Student Character: {chosenCharacter.emoji}</span>
                         ) : (
                             <span>Optional: Student Character</span>
                         )}
-                        <Picker onEmojiClick={onEmojiClick} />
+                        <Picker onEmojiClick={onEmojiClick}/>
                     </div>
                     <br/>
                     <input type="button" value="Add Student" onClick={handleManualAdd}/>
@@ -129,6 +139,7 @@ export default function AddStudents(props) {
             <div>
                 <h3>Upload Roster CSV:</h3>
                 <p>CSV should have the following columns: "Name" or "Student", (optional) "Animal"</p>
+                <p>Name/Student column should be in the format: "Last, First" or "Last, First Middle"</p>
                 <CSVReader
                     ref={buttonRef}
                     onDrop={handleOnDrop}
@@ -140,7 +151,7 @@ export default function AddStudents(props) {
                         header: true,
                         transformHeader: function (h) {
                             let header = h.toLowerCase();
-                            if(header === 'student' || header === ['student name']) header = 'name';
+                            if (header === 'student' || header === ['student name']) header = 'name';
                             return header
                         }
                     }}
