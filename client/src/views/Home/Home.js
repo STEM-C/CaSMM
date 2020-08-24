a58faa595c5796dd27de31ad56a0ba5eed1a39ec
Adam Tamargo <tamargoadam@gmail.com>
2020-08-24 16:05:41 -0400

    add about to nav bar

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -15,9 +15,13 @@ export default function Home(props) {
         history.push('/sandbox')
     };
 
+    const handleAbout = () => {
+        history.push('/about')
+    };
+
     return(
         <div className='container nav-padding'>
-            <NavBar handleTeacherLogin={handleTeacherLogin} handleSandbox={handleSandbox}/>
+            <NavBar handleTeacherLogin={handleTeacherLogin} handleSandbox={handleSandbox} handleAbout={handleAbout}/>
             <div id='join-wrapper'>
                 <img src={Logo} id='casmm-logo' alt='logo'/>
                 <HomeJoin history={props.history}/>
