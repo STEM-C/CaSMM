194cf1d5e27de8efbe20db3bed43e0b38b2c2ccf
Adam Tamargo <tamargoadam@gmail.com>
2020-08-24 15:56:43 -0400

    add 'How it Works' to about

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -4,7 +4,6 @@ import NSF from "../../assets/nsf_logo.png"
 import UF from "../../assets/uf_logo.png"
 import TAMU from "../../assets/tamu_logo.png"
 import NavBar from "../../components/NavBar/NavBar";
-import Logo from "../../assets/casmm_logo.png";
 
 export default function About(props) {
     const {history} = props;
@@ -25,7 +24,9 @@ export default function About(props) {
         <div className='container nav-padding'>
             <NavBar handleTeacherLogin={handleTeacherLogin} handleSandbox={handleSandbox} handleHome={handleHome}/>
             <div id='about-content-container'>
-                <h1 id='title'>About CASMM</h1>
+                <h1 id='title'>
+                    About CASMM
+                </h1>
                 <div id='logos' class="flex space-between">
                     <img src={UF} alt='uf'/>
                     <img src={NSF} alt='nsf'/>
@@ -37,11 +38,24 @@ export default function About(props) {
                 </p>
                 <p>
                     CASMM, or Computation and Science Modeling through Making, is a cloud-based programming interface
-                    designed for fifth and sixth grade children to help support them in building computational physical
+                    designed for fifth and sixth grade students to support them in building computational physical
                     models for science experiments in the classroom.
                 </p>
+                <div id='divider'/>
+                <h1 id='secondary-title'>
+                    How it Works
+                </h1>
+                <p>
+                    CASMM makes use of Google's <a href='https://developers.google.com/blockly'>Blockly</a> JavaScript
+                    library to build a block based visual programming editor within our user interface. Programs created
+                    through the visual programming editor can be compiled into Arduino executable code and uploaded
+                    directly onto a students Arduino. These programs, once uploaded to the student's Arduino, provide
+                    support in performance of a corresponding science project in the classroom.
+                </p>
                 <div id="divider"/>
-                <h1 id='secondary-title'>Our Team</h1>
+                <h1 id='secondary-title'>
+                    Our Team
+                </h1>
                 <p>
                     The following team members from the University of Florida have contributed to the design,
                     development, and evaluation of the CASMM application:
