16c4d83cc1d8156314d9a04bbdc6889fff6e902d
syuchen1 <siyuchen1@ufl.edu>
2021-08-29 23:47:25 -0400

    fix classroom manager api bug

--- server/api/classroom-manager/services/classroom-manager.js
+++ server/api/classroom-manager/services/classroom-manager.js
@@ -2,29 +2,26 @@
 
 // find a classroom manager by user id
 module.exports.findById = async (id) => {
-
-    const [ mentor, teacher ] = await Promise.all(['mentor', 'teacher'].map(async type => 
-        strapi.services[type].findOne({ user: id })
-    ))
-
-    return {
-        classroomManager: mentor ? mentor : teacher,
-        type: mentor ? 'mentor' : 'teacher'
-    }
-}
+  const mentor = await strapi.services.mentor.findOne({ user: id });
+  return {
+    classroomManager: mentor,
+  };
+};
 
 // find all classes for a classroom manager
 module.exports.findClassesById = async (id) => {
+  const mentor = await strapi.services.mentor.findOne({ user: id });
+  const classrooms = mentor.classrooms;
 
-    const [ mentor, teacher ] = await Promise.all(['mentor', 'teacher'].map(async type =>
-        strapi.services[type].findOne({ user: id })
-    ));
-    const classrooms = mentor ? mentor.classrooms : teacher.classrooms;
+  const crList = await Promise.all(
+    classrooms.map(async (classroom) => {
+      const model = await strapi
+        .query('classroom')
+        .model.where('id', classroom.id)
+        .fetch();
+      return model ? model.toJSON() : undefined;
+    })
+  );
 
-    const crList = await Promise.all(classrooms.map(async classroom => {
-        const model = await strapi.query('classroom').model.where('id', classroom.id).fetch();
-        return model ? model.toJSON() : undefined;
-    }));
-
-    return crList
-}
+  return crList;
+};
