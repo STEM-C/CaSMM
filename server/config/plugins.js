f1bf540711ff18c5fe95e1f82a60695f4bbdfcac
syuchen1 <siyuchen1@ufl.edu>
2021-10-27 18:22:00 -0400

    added sentry

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,22 +1,22 @@
-require('dotenv').config({ path: '../.env' })
-
+require('dotenv').config({ path: '../.env' });
 
 module.exports = () => ({
-    // ...
-    email: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: 'smtp-relay.sendinblue.com',
-        port: 587,
-        auth: {
-          user: process.env.EMAIL_SMTP_USER,
-          pass: process.env.EMAIL_SMTP_PASS,
-        },
-      },
-      settings: {
-        defaultFrom: 'no-reply@casmm.org',
-        defaultReplyTo: 'no-reply@casmm.org',
+  email: {
+    provider: 'nodemailer',
+    providerOptions: {
+      host: 'smtp-relay.sendinblue.com',
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_SMTP_USER,
+        pass: process.env.EMAIL_SMTP_PASS,
       },
     },
-    // ...
-  })
+    settings: {
+      defaultFrom: 'no-reply@casmm.org',
+      defaultReplyTo: 'no-reply@casmm.org',
+    },
+  },
+  sentry: {
+    dsn: 'https://1211be7d90e04b218083d8a8b760e4d6@o1050432.ingest.sentry.io/6031665',
+  },
+});
