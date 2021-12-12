60b2628fd686bb32e3958f3ef79515d063f38546
Siyu Chen <siyuchen1@ufl.edu>
2021-12-12 01:33:05 -0500

    updated cc workspace controller

--- server/api/block/services/block.js
+++ server/api/block/services/block.js
@@ -1,46 +1,68 @@
-'use strict'
+'use strict';
 
-const sortCategory = (toolbox) =>{
-    const order = ['Logic', 'Control', 'Math', 'Text', 'Variables', 'Functions', 'IO', 'Time', 'Audio', 'Motors', 'Comms'];
+const sortCategory = (toolbox) => {
+  const order = [
+    'Logic',
+    'Control',
+    'Math',
+    'Text',
+    'Variables',
+    'Functions',
+    'IO',
+    'Time',
+    'Audio',
+    'Motors',
+    'Comms',
+  ];
 
-    const sorted = toolbox.sort(function(a,b) {
-        return order.indexOf(a[0]) - order.indexOf(b[0]);
-    })
-    return sorted;
-}
+  const sorted = toolbox.sort(function (a, b) {
+    return order.indexOf(a[0]) - order.indexOf(b[0]);
+  });
+  return sorted;
+};
 
 // get all the blocks for a day
 module.exports.findByDay = async (id) => {
+  // get the target day
+  const day = await strapi.services.day.findOne({ id }, [
+    'blocks.blocks_category',
+  ]);
 
-    // get the target day
-    const day = await strapi.services.day.findOne({ id }, ['blocks.blocks_category'])
+  // return the blocks only if the day is found
+  return day ? day.blocks : undefined;
+};
 
-    // return the blocks only if the day is found
-    return day ? day.blocks : undefined
-}
+// get all the blocks for a cc workspace
+module.exports.findByWorkspace = async (id) => {
+  // get the target day
+  const workspace = await strapi.services['cc-workspace'].findOne({ id }, [
+    'blocks.blocks_category',
+  ]);
+
+  // return the blocks only if the day is found
+  return workspace ? workspace.blocks : undefined;
+};
 
 // create a blockly friendly toolbox from blocks
 module.exports.blocksToToolbox = (blocks) => {
+  let toolbox = {};
+  blocks.forEach((block) => {
+    // validate the block fields
+    const { blocks_category, name, description, image_url } = block;
+    if (!blocks_category) return;
 
-    let toolbox = {};
-    blocks.forEach(block => {
+    // only take the required fields from the block
+    let sanitizedBlock = { name, description, image_url };
 
-        // validate the block fields
-        const { blocks_category, name, description, image_url } = block
-        if (!blocks_category) return 
+    // append the block to an existing category
+    // else create a new category
+    if (toolbox[blocks_category.name]) {
+      toolbox[blocks_category.name].push(sanitizedBlock);
+    } else {
+      toolbox[blocks_category.name] = [sanitizedBlock];
+    }
+  });
+  const arr = Object.entries(toolbox);
 
-        // only take the required fields from the block
-        let sanitizedBlock = { name, description, image_url }
-
-        // append the block to an existing category
-        // else create a new category
-        if (toolbox[blocks_category.name]) {
-            toolbox[blocks_category.name].push(sanitizedBlock)
-        } else {
-            toolbox[blocks_category.name] = [sanitizedBlock]
-        }
-    })
-    const arr = Object.entries(toolbox)
-    
-    return sortCategory(arr);
-}
+  return sortCategory(arr);
+};
--- server/api/cc-workspace/controllers/cc-workspace.js
+++ server/api/cc-workspace/controllers/cc-workspace.js
@@ -3,19 +3,17 @@
 const { sanitizeEntity } = require('strapi-utils/lib');
 
 module.exports = {
-  // Update day template and block list
-  async update(ctx) {
-    // find the day
-    const { id } = ctx.params;
-    let workspace = await strapi.services[cc - workspace].findOne({ id: id });
-    if (!workspace)
-      return ctx.notFound('Invalid workspace id', {
-        id: 'day.id.invalid',
-        error: 'ValidationError',
-      });
-
-    // update template and blocks
-    day.template = ctx.request.body.template;
+  // Create workspace template and block list
+  async create(ctx) {
+    let workspace = {
+      name: '',
+      description: '',
+      template: '',
+      blocks: [],
+    };
+    workspace.template = ctx.request.body.template;
+    workspace.name = ctx.request.body.name;
+    workspace.description = ctx.request.body.description;
     let unfriendlyBlocks = ctx.request.body.blocks;
     let friendlyBlocks = [];
     for (let i = 0; i < unfriendlyBlocks.length; i++) {
@@ -24,17 +22,52 @@ module.exports = {
       });
       friendlyBlocks.push(currentBlock);
     }
-    day.blocks = friendlyBlocks;
+    workspace.blocks = friendlyBlocks;
 
-    const updatedDay = await strapi.services.day.update({ id: id }, day);
-    return sanitizeEntity(updatedDay, { model: strapi.models.day });
+    const createdWorkspace = await strapi.services['cc-workspace'].create(
+      workspace
+    );
+    return sanitizeEntity(createdWorkspace, {
+      model: strapi.models['cc-workspace'],
+    });
+  },
+  // Update workspace template and block list
+  async update(ctx) {
+    // find the day
+    const { id } = ctx.params;
+    let workspace = await strapi.services['cc-workspace'].findOne({ id: id });
+    if (!workspace)
+      return ctx.notFound('Invalid workspace id', {
+        id: 'cc-workspace.id.invalid',
+        error: 'ValidationError',
+      });
+
+    // update template and blocks
+    workspace.template = ctx.request.body.template;
+    let unfriendlyBlocks = ctx.request.body.blocks;
+    let friendlyBlocks = [];
+    for (let i = 0; i < unfriendlyBlocks.length; i++) {
+      let currentBlock = await strapi.services.block.findOne({
+        name: unfriendlyBlocks[i],
+      });
+      friendlyBlocks.push(currentBlock);
+    }
+    workspace.blocks = friendlyBlocks;
+
+    const updatedWorkspace = await strapi.services['cc-workspace'].update(
+      { id: id },
+      workspace
+    );
+    return sanitizeEntity(updatedWorkspace, {
+      model: strapi.models['cc-workspace'],
+    });
   },
 
   async toolbox(ctx) {
     const { id } = ctx.params;
 
     // get the blocks
-    const blocks = await strapi.services.block.findByDay(id);
+    const blocks = await strapi.services.block.findByWorkspace(id);
 
     // return 404 if blocks is undefined
     // (only the case of a day not existing)
