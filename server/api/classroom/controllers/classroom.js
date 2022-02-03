140afbb9857f919b4286ddd615ca40989dcb7762
chensation <fogwhite99@gmail.com>
2022-02-02 22:36:45 -0500

    mentor saved workspaces tab viewable

--- server/api/classroom/controllers/classroom.js
+++ server/api/classroom/controllers/classroom.js
@@ -123,22 +123,11 @@ module.exports = {
 
   async workspaces(ctx) {
     const { id } = ctx.params;
-
-    const classroom = await strapi.services.classroom.findOne(id);
+    const classroom = await strapi.services.classroom.findOne({id: id});
     // check if the classroom exists
     let response;
     if (classroom) {
       response = classroom.cc_workspaces;
-      // const workspaces = classroom.cc_workspaces;
-      // // respond with a list of cc_workspaces
-      // response = workspaces.map((workspace) => {
-      //   return {
-      //     id: workspace.id,
-      //     name: workspace.name,
-      //     description: workspace.description,
-      //     template: workspace.template
-      //   };
-      // });
     }
 
     return response;
