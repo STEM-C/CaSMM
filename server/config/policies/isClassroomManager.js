7ee4f076c3a1b8820429b015ba251d960af7bcf5
Siyu Chen <siyuchen1@ufl.edu>
2022-02-08 19:08:00 -0500

    researcher bypass auth

--- server/config/policies/isClassroomManager.js
+++ server/config/policies/isClassroomManager.js
@@ -2,10 +2,13 @@
 // Check if the current user is a classroom manager
 //
 module.exports = async (ctx, next) => {
-    if (ctx.state.user && ctx.state.user.role.name === 'Classroom Manager') {
-        // Go to next policy or controller
-        return await next()
-    }
-  
-    ctx.unauthorized(`You're not allowed to perform this action!`)
-}
+  if (
+    (ctx.state.user && ctx.state.user.role.name === 'Classroom Manager') ||
+    ctx.state.user.role.name === 'Researcher'
+  ) {
+    // Go to next policy or controller
+    return await next();
+  }
+
+  ctx.unauthorized(`You're not allowed to perform this action!`);
+};
