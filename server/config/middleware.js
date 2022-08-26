4de31c187162afeb582e507837bde0b771dae94f
mikelxk <mike.lxk@gmail.com>
2022-08-26 18:31:12 -0400

    use brotil again

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -20,7 +20,7 @@ module.exports = {
         gzip: {
             enabled: true,
             options: {
-              br: false
+                br: { threshold: 100 },
             }
         }
         // logger: {
