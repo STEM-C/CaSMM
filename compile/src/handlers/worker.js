95c4f8166d4490b16b1581d0ab827e2d5424a4c3
Mike Li <mike.lxk@gmail.com>
2022-09-17 00:17:11 -0400

    t

--- compile/src/handlers/worker.js
+++ compile/src/handlers/worker.js
@@ -1,43 +1,29 @@
-const { processJob } = require('../controllers/job')
-const { compileLog } = require('../utils/base')
+const { processJob } = require("../controllers/job")
+const { compileLog } = require("../utils/base")
 
-const Queue = require('bull')
-const redisUrlParse = require('redis-url-parse')
+const Queue = require("bull")
 
 // Connect to a local redis instance locally, and the Heroku-provided URL in production
-const REDIS_URL = process.env.REDIS_URL || 'redis://compile_queue:6379'
-const { host, port, password } = redisUrlParse(REDIS_URL);
-const bullOptions = REDIS_URL.includes('rediss://')
-  ? {
-      redis: {
-        port: Number(port),
-        host,
-        password,
-        tls: {
-          rejectUnauthorized: false,
-          requestCert: true,
-        },
-      },
-    }
-  : REDIS_URL;
+const REDIS_URL = process.env.REDIS_URL || "redis://compile_queue:6379"
+
 // The maximum number of jobs each worker should process at once
 // Each job is CPU-intensive, so this value should not be too high
 const maxJobsPerWorker = process.env.JOB_CONCURRENCY || 1
 
 module.exports.init = () => {
-
-    // starting up the service
-    compileLog('Starting compile cluster')
+  // starting up the service
+  compileLog("Starting compile cluster")
 }
 
-module.exports.start = (id) => {
+module.exports.start = id => {
+  // Signal worked started
+  compileLog(`Started worker ${id}`)
 
-    // Signal worked started
-    compileLog(`Started worker ${ id }`);
+  // Connect to the named queue
+  const compile_queue = new Queue("submissions", REDIS_URL, {
+    redis: { tls: { rejectUnauthorized: false, requestCert: true } },
+  })
 
-    // Connect to the named queue
-    const compile_queue = new Queue('submissions', bullOptions)
-
-    // start processing jobs from the submission queue
-    compile_queue.process(maxJobsPerWorker, processJob)
+  // start processing jobs from the submission queue
+  compile_queue.process(maxJobsPerWorker, processJob)
 }
