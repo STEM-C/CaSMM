3ad382262f52f57c0d4c53c2cbd3c65d6c04806d
Mike Li <mike.lxk@gmail.com>
2022-10-29 18:19:43 -0400

    feat: add more lazy load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,6 @@ import PrivateRoute from './Utils/PrivateRoute';
 import BlocklyPage from './views/BlocklyPage/BlocklyPage';
 import ContentCreator from './views/ContentCreator/ContentCreator';
 import Home from './views/Home/Home';
-import Classroom from './views/Mentor/Classroom/Classroom';
-import Dashboard from './views/Mentor/Dashboard/Dashboard';
 import Replay from './views/Replay/Replay';
 import DayLevelReport from './views/Researcher/DayLevelReport';
 import DayLevelReportView from './views/Researcher/DayLevelReportView';
@@ -14,13 +12,15 @@ import GroupReport from './views/Researcher/GroupReport';
 import Report from './views/Researcher/Report';
 import Student from './views/Student/Student';
 import ForgetPassword from './views/TeacherLogin/ForgetPassword';
-//lazy load some components
+//lazy loaded components
 const About = loadable(() => import('./views/About/About'))
 const NotFound = loadable(() => import('./views/NotFound'))
 const TeacherLogin = loadable(() => import('./views/TeacherLogin/TeacherLogin'))
 const StudentLogin = loadable(() => import('./views/StudentLogin/StudentLogin'))
 const BugReport = loadable(() => import('./views/BugReport/BugReport'))
 const ResetPassword = loadable(() => import('./views/TeacherLogin/ResetPassword'))
+const Dashboard = loadable(() => import('./views/Mentor/Dashboard/Dashboard'))
+const Classroom = loadable(() => import('./views/Mentor/Classroom/Classroom'))
 
 const App = () => {
   return (
