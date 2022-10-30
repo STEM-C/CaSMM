d917006d3837dd7017c7cea5f0c68fa76eb9056f
Mike Li <mike.lxk@gmail.com>
2022-10-30 02:29:16 -0400

    revert change in esbuild

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite"
 const base = process.env.PUBLIC_URL ?? "/"
 
 export default defineConfig({
-  optimizeDeps: {
-    disabled: false,
-  },
   css: {
     preprocessorOptions: {
       less: {
@@ -42,6 +39,5 @@ export default defineConfig({
   },
   build: {
     outDir: "build",
-    commonjsOptions: { include: [] },
   },
 })
