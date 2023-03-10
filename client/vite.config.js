92672778eacc63f7369f2a1c5c6f1b802b6112e3
mikelxk <mike.lxk@gmail.com>
2023-03-10 18:11:51 -0500

    row back to antdv4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,5 @@
-import { convertLegacyToken } from "@ant-design/compatible"
 import react from "@vitejs/plugin-react"
-import { theme } from "antd/lib"
 import { defineConfig } from "vite"
-const { defaultAlgorithm, defaultSeed } = theme
-
-const mapToken = defaultAlgorithm({
-  ...defaultSeed,
-  colorPrimary: "#3d5c82",
-  colorSuccess: "#52c41a",
-  colorWarning: "#faad14",
-})
-const v4Token = convertLegacyToken(mapToken)
 
 const base = process.env.PUBLIC_URL ?? "/"
 
@@ -18,7 +7,21 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
-        modifyVars: v4Token,
+        modifyVars: {
+          "@primary-color": "#3d5c82", // primary color for all components
+          "@link-color": "#3d5c82", // link color
+          "@success-color": "#52c41a", // success state color
+          "@warning-color": "#faad14", // warning state color
+          "@error-color": "#f5222d", // error state color
+          "@font-size-base": "14px", // major text font size
+          "@heading-color": "rgba(0, 0, 0, 0.85)", // heading text color
+          "@text-color": "rgba(0, 0, 0, 0.65)", // major text color
+          "@text-color-secondary": "rgba(0, 0, 0, 0.45)", // secondary text color
+          "@disabled-color": "rgba(0, 0, 0, 0.25)", // disable state color
+          "@border-radius-base": "4px", // major border radius
+          "@border-color-base": "#d9d9d9", // major border color
+          "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.15)", // major shadow for layers
+        },
         javascriptEnabled: true,
       },
     },
