e5248c85f56bd31cedbd634ef463d9f93fa3ae4e
Carter Mayeux <159168644+cmayeux05@users.noreply.github.com>
2024-10-18 17:22:03 -0500

    change plugins

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -18,6 +18,14 @@ module.exports = () => ({
       defaultReplyTo: 'no-reply@casmm.org',
     },
   },
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        //sizeLimit: 1000000, // Set the size limit for uploads (in bytes)
+      },
+    },
+  },
   sentry: {
     dsn: process.env.SENTRY_DNS || '',
     integrations: [new Sentry.Integrations.Http({ tracing: true })],
