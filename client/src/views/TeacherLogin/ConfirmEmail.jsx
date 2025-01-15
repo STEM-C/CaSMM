45ed437125045602adf4828f939f7a9eedd1c598
Carter Mayeux <159168644+cmayeux05@users.noreply.github.com>
2025-01-15 17:16:56 -0600

    fix it now

--- client/src/views/TeacherLogin/ConfirmEmail.jsx
+++ client/src/views/TeacherLogin/ConfirmEmail.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { confirmEmail } from '../../Utils/requests';
 import NavBar from '../../components/NavBar/NavBar';
-import "./Sorry.less";
+import './Sorry.less';
+
 
 export default function ConfirmEmail() {
     const search = useLocation().search;
-    const code = new URLSearchParams(search).get('confirmation');
-    //console.log(code)
-    confirmEmail(code)
-        .then((response) => {
-            console.log(response);
-        });
+    const [code, setCode] = useState(null);
+    useEffect(() => {
+      const queryParams = new URLSearchParams(search);
+      const confirmationCode = queryParams.get('confirmation');
+      setCode(confirmationCode);  // Store the code in state
+      //console.log(confirmationCode);  // Debug: Check if the code is extracted correctly
+
+      // If a code exists, make the API call
+      if (confirmationCode) {
+          confirmEmail(confirmationCode)
+              .then((response) => {
+                  console.log(response);
+              })
+              .catch((error) => {
+                  console.error("Error confirming email:", error);
+              });
+      }
+  }, [search]); // Only trigger when the search string changes
     return (
     <div className="container nav-padding">
       <NavBar />
